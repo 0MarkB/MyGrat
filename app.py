03f43baa1b7c1fa7a63a1cff8a78b71ba4a4d38d
Morde <boukrismordehai@gmail.com>
2023-10-17 22:03:52 -0400

    added result window (show result button); added better creation of excel and naming of it. ( save results button.)

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox, QTableWidgetItem
 import sys
 import traceback
 
 from MyGratMain import Ui_MainWindow  # Importing your UI class
+from ResultWindow import Ui_ResultWindow  # Importing your Result Window
 
 # Import your logic functions here
 from mainWeekly import (
@@ -15,6 +16,21 @@ from mainWeekly import (
 )
 
 
+class ResultsDialog(QMainWindow, Ui_ResultWindow):
+    def __init__(self, data, parent=None):
+        super(ResultsDialog, self).__init__(parent)
+        self.setupUi(self)
+        self.populate_table(data)
+
+    def populate_table(self, data):
+        self.resultsTable.setRowCount(len(data))
+        self.resultsTable.setColumnCount(2)
+        self.resultsTable.setHorizontalHeaderLabels(["Employee", "Weekly Tips"])
+        for row, (employee, tip) in enumerate(data.items()):
+            self.resultsTable.setItem(row, 0, QTableWidgetItem(employee))
+            self.resultsTable.setItem(row, 1, QTableWidgetItem(str(tip)))
+
+
 class MyApp(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MyApp, self).__init__()
@@ -24,6 +40,7 @@ class MyApp(QMainWindow, Ui_MainWindow):
         self.pushButton.clicked.connect(self.upload_time_entries)
         self.pushButton_2.clicked.connect(self.upload_orders)
         self.pushButton_3.clicked.connect(self.distribute_tips_weekly)
+        self.pushButton_4.clicked.connect(self.show_results)  # Connect the "Show Results" button
         self.pushButton_5.clicked.connect(self.save_results)
 
         # Variables to store file paths
@@ -34,15 +51,15 @@ class MyApp(QMainWindow, Ui_MainWindow):
         filepath, _ = QFileDialog.getOpenFileName(self, "Select Time Entries CSV File", "",
                                                   "CSV files (*.csv);;All files (*)")
         if filepath:
-            self.time_entries_file_path = filepath  # Fixed here
-            self.label_6.setText("Time entries file uploaded successfully!")
+            self.time_entries_file_path = filepath
+            self.statusbar.showMessage("Time entries file uploaded successfully!")
 
     def upload_orders(self):
         filepath, _ = QFileDialog.getOpenFileName(self, "Select Orders CSV File", "",
                                                   "CSV files (*.csv);;All files (*)")
         if filepath:
-            self.orders_file_path = filepath  # Fixed here
-            self.label_6.setText("Orders file uploaded successfully!")
+            self.orders_file_path = filepath
+            self.statusbar.showMessage("Orders file uploaded successfully!")
 
     def distribute_tips_weekly(self):
         try:
@@ -62,17 +79,24 @@ class MyApp(QMainWindow, Ui_MainWindow):
                 role_pool_points,
                 time_entries_df
             )
-            self.label_6.setText("Tips distributed successfully!")
+            self.statusbar.showMessage("Tips distributed successfully!")
         except Exception as e:
             error_message = f"An error occurred: {str(e)}\n\n{traceback.format_exc()}"
             self.ErrorTracebackBox.setText(error_message)
+
+    def show_results(self):
+        if hasattr(self, 'employee_weekly_cuts'):
+            self.results_dialog = ResultsDialog(self.employee_weekly_cuts, self)
+            self.results_dialog.show()
+        else:
+            QMessageBox.warning(self, "No Results", "Please distribute the tips first.")
 
     def save_results(self):
         try:
             output_data = [{"Employee": key, "Weekly Tips": value} for key, value in self.employee_weekly_cuts.items()]
             output_file = 'employee_weekly_results.xlsx'
             write_excel_data(output_file, output_data, 'EmployeeWeeklyCuts')
-            self.label_6.setText(f"Employee weekly cuts have been saved to {output_file}.")
+            self.statusbar.showMessage(f"Employee weekly cuts have been saved to {output_file}.")
         except Exception as e:
             error_message = f"An error occurred: {str(e)}\n\n{traceback.format_exc()}"
             self.ErrorTracebackBox.setText(error_message)
